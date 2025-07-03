a479d519c4b902f2b9fc5d5c95b27f9aa5f3df5b
Juan esteban <juan.collazos3@soy.sena.edu.co>
2025-07-02 23:22:14 -0500

    feat: Setup initial infrastructure and backend services

---
+++ Backend/product-service/src/test/java/com/nexusgear/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusgear.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Backend/user-auth-service/src/test/java/com/nexusgear/userauthservice/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusgear.userauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
